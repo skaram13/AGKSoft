4d9ef5e0baecb3201578f668d413567f91285dac
Samantha Karam <skaram13st@gmail.com>
2019-07-06 16:57:08 -0400

    AGKSoft Recovery Video

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rem This must run from the F:\website\agksoft\front-end folder using a Command Prompt
+rem This must run from the C:\website\agksoft\front-end folder using a Command Prompt
 
 echo Creating ReBuild Log on c:\temp\rebuild.log
 echo %DATE% >c:\temp\rebuild.log
@@ -11,24 +11,24 @@ cd build
 echo.
 echo Finished Building Static Web-Site...
 echo.
-echo Copying Part 1 to w:
-copy *.* w: /Y >>c:\temp\rebuild.log
+echo Copying Part 1 to \\agk-web1\wwwroot
+copy *.* \\agk-web1\wwwroot /Y >>c:\temp\rebuild.log
 
 cd static
 
-echo Copying Part 2 to w: (static CSS)
+echo Copying Part 2 to \\agk-web1\wwwroot (static CSS)
 cd css
-copy *.* w:\static\css /Y >>c:\temp\rebuild.log
+copy *.* \\agk-web1\wwwroot\static\css /Y >>c:\temp\rebuild.log
 cd..
 
-echo Copying Part 3 to w: (static JS)
+echo Copying Part 3 to \\agk-web1\wwwroot (static JS)
 cd js
-copy *.* w:\static\js /Y >>c:\temp\rebuild.log
+copy *.* \\agk-web1\wwwroot\static\js /Y >>c:\temp\rebuild.log
 cd..
 
-echo Copying Part 4 to w: (static MEDIA)
+echo Copying Part 4 to \\agk-web1\wwwroot (static MEDIA)
 cd media
-copy *.* w:\static\media /Y >>c:\temp\rebuild.log
+copy *.* \\agk-web1\wwwroot\static\media /Y >>c:\temp\rebuild.log
 cd..
 
 echo Back to static folder...
