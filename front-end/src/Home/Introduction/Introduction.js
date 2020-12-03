78ab1c85b3ec3807eca4baf2b078cdc714c09ba0
Samantha Karam <skaram13st@gmail.com>
2020-12-03 12:43:38 -0500

    EDI Offer

--- front-end/src/Home/Introduction/Introduction.js
+++ front-end/src/Home/Introduction/Introduction.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Introduction.css';
 import logo from '../../assets/AGKlogo.png';
+import edioffer from '../../assets/AGKEDISmall.jpg';
 
 class Introduction extends Component {
     render() {
@@ -13,9 +14,14 @@ class Introduction extends Component {
                         </td>
                         <td className="logo-spacing">
                             <tr><div className="home-heading-text">AGKSoft</div></tr>
-                            <tr><div className="description-text">AGKSoft offers the C-Store industry a product that is able to cater to every stage of running a C-Store.</div></tr>
+                            <tr><div className="description-text">AGKSoft Back Office Software, Point Of Sale and Home Office.</div></tr>
                             <tr align="left"><a href="/contact"><button className="sales-button">CONTACT A SALES REP</button></a></tr>
                         </td>
+
+                        <td className="logo-spacing">
+                            <tr align="left"><img className="edi-offer" src={edioffer} alt="AGKSoft Unlimited EDI Offer"/></tr>
+                        </td>
+
                     </table>
                 </div>
             </div>
