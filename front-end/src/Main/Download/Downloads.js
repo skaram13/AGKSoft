c0643ca3f8cc42ad7a71342d1d0f26bed76a34ec
Samantha Karam <skaram13st@gmail.com>
2020-12-23 15:52:59 -0500

    UGreen

--- front-end/src/Main/Download/Downloads.js
+++ front-end/src/Main/Download/Downloads.js
@@ -347,7 +347,10 @@ class Downloads extends Component {
                                         <td><a href={baseUrl + "Install_PDFR_Pro_v228.exe"}>Download PDF Writer</a></td>
                                     </tr>
                                     <tr>
-                                        <td><a href={baseUrl + "PL23XX-M_LogoDriver_Setup_v200_20190815.exe"}>TrendNet TU-S9 Prolific USB-to-Serial Driver</a></td>
+                                        <td><a href={baseUrl + "PL23XX-M_LogoDriver_Setup_v200_20190815.exe"}>TrendNet TU-S9 USB-to-Serial Prolific Driver</a></td>                                        
+                                    </tr>                                    
+                                    <tr>
+                                        <td><a href={baseUrl + "PL23XX-M_LogoDriver_Setup_v203_20201029.exe"}>UGreen USB 2 Serial Prolific Driver</a></td>
                                     </tr>                                    
                                     <tr>
                                         <td>
