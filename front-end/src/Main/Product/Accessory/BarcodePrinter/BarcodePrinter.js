89d1fd9f7a6bc2005a0770980692ecbd55ff1310
Samantha Karam <skaram13st@gmail.com>
2021-03-18 14:20:01 -0400

    Dymo

--- front-end/src/Main/Product/Accessory/BarcodePrinter/BarcodePrinter.js
+++ front-end/src/Main/Product/Accessory/BarcodePrinter/BarcodePrinter.js
@@ -15,25 +15,24 @@ class BarcodePrinter extends Component {
                 <Header/>
                 <div className="main-container">
                     <h1 className="heading-text">AGKSoft Barcode Printer</h1>
-                    <div className="description-text">Shown below are some barcode samples that you can print.  You can print on [2" X 0.75"]  or  [3.5" X 1.1"] or  [4" X 2.31"] or any other custom size you want</div>
-                    <div className="description-text">The portable can be used to create your price book, scan your Inventory or Invoices as well as take your orders if needed.</div>
+                    <div className="description-text">You can print on Tiny [2"X0.75"], Small [3.5"X1.1"] or Large [4"X2.31"] labels.</div>
+                    <div className="description-text">You can order any of these labels by contacting AGKSoft.</div>
 
                     <table className="product-entry-spacing row-centered center-block">
-                        <th className="center-header">Figure 1 (4" X 2.31")</th>
-                        <tr className="center-images"><img alt='Figure 1 (4" X 2.31")' className="product-images image-border" src={barcode1}/></tr>
-                    </table>
-                    <table className="product-entry-spacing row-centered center-block">
-                        <th className="center-header">Figure 2 (4" X 2.31")</th>
-                        <tr className="center-images"><img alt='Figure 2 (4" X 2.31")' className="product-images image-border" src={barcode2}/></tr>
-                    </table>
-                    <table className="product-entry-spacing row-centered center-block">
-                        <th className="center-header">Figure 3 (3.5" X 1.1")</th>
-                        <tr className="center-images"><img alt='Figure 3 (3.5" X 1.1")' className="product-images image-border" src={barcode3}/></tr>
-                    </table>
-                    <table className="product-entry-spacing row-centered center-block">
-                        <th className="center-header">Figure 4 (2" X 0.75")</th>
+                        <th className="center-header">Figure 1 - Tiny (2" X 0.75")</th>
                         <tr className="center-images"><img alt='Figure 4 (2" X 0.75")' className="product-images image-border" src={barcode4}/></tr>
                     </table>
+
+                    <table className="product-entry-spacing row-centered center-block">
+                        <th className="center-header">Figure 2 - Small (3.5" X 1.1")</th>
+                        <tr className="center-images"><img alt='Figure 3 (3.5" X 1.1")' className="product-images image-border" src={barcode3}/></tr>
+                    </table>
+
+                    <table className="product-entry-spacing row-centered center-block">
+                        <th className="center-header">Figure 3 - Large (4" X 2.31")</th>
+                        <tr className="center-images"><img alt='Figure 1 (4" X 2.31")' className="product-images image-border" src={barcode1}/></tr>
+                    </table>
+
                 </div>
                 <Footer/>
             </div>
