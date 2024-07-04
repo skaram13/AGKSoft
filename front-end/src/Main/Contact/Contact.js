36dbf642488a137c8b6314f71a6b4a0f280ebfc8
Samantha Karam <skaram13st@gmail.com>
2024-07-03 21:57:16 -0400

    removed contact email

--- front-end/src/Main/Contact/Contact.js
+++ front-end/src/Main/Contact/Contact.js
@@ -11,19 +11,6 @@ class Contact extends Component {
                 <Header/>
                 <div className="main-container">
                     <div className="main-heading row-centered">Contact</div>
-                    <table className="table-one">
-                        <div className="contact-form">
-                            <h4 className="row">AGKSoft is here ready to serve you with all your software needs, so feel free to call or email any time.</h4>
-                            <form method="POST" action="emailagksoft.asp" accept-charset="UTF-8">
-                                <div className="row"><input type="text" required="" placeholder="Name" id="fromName" name="fromWho" className="input-boxes"/></div>
-                                <div className="row"><input type="email" required="" placeholder="Email address" id="fromemail" name="fromemail" className="input-boxes"/></div>
-                                <div className="row"><input type="tel" required="" placeholder="Phone number" id="phone" name="phone" className="input-boxes"/></div>
-                                <div className="row"><input type="text" required="" placeholder="Subject" id="subject"  name="subject" className="input-boxes"/></div>
-                                <div className="row"><textarea required="" placeholder="Message" className="message-box input-boxes" id="messageBody" name="Body"/></div>
-                                <div className="row"><input type="submit" value="SEND MAIL" className="submit-btn"/></div>
-                            </form>
-                        </div>
-                    </table>
                     <table className="table-two">
                         <h4 className="row">When you call, your call is never routed to another country, all provided numbers are in the United States.
                             Email support is monitored by all Support & Development Engineers.</h4>
